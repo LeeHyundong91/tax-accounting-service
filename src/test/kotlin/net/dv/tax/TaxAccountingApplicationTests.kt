8129ef6ada7459b410cb937cfec6e28887fbed0d
mino <dev.minokim@gmail.com>
2022-12-22 12:20:11 +0900

    first commit

---
+++ src/test/kotlin/net/dv/tax/TaxAccountingApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.dv.tax
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaxAccountingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
