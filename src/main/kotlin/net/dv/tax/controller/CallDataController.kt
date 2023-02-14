2c2dbf4d207cc94f5d69f3e3c08ff8bf43d2538e
mino <dev.minokim@gmail.com>
2023-02-14 15:28:47 +0900

    #2 add  - medical benefits module

--- src/main/kotlin/net/dv/tax/controller/CallDataController.kt
+++ src/main/kotlin/net/dv/tax/controller/CallDataController.kt
@@ -1,22 +1,23 @@
 package net.dv.tax.controller
 
 import mu.KotlinLogging
-import net.dv.tax.dto.sales.MedicalBenefitsListDto
 import net.dv.tax.service.common.TaxDataLoadAndSaveService
 import net.dv.tax.service.sales.MedicalBenefitsService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class CallDataController(private val taxDataLoadService: TaxDataLoadAndSaveService,
-    private val medicalBenefitsService: MedicalBenefitsService) {
+class CallDataController(
+    private val taxDataLoadService: TaxDataLoadAndSaveService,
+    private val medicalBenefitsService: MedicalBenefitsService,
+) {
     private val log = KotlinLogging.logger {}
 
     @GetMapping("/get/data")
-    fun getTaxDataFromReceiveService(): List<MedicalBenefitsListDto>{
-//        taxDataLoadService.getBenefitsData()
-        log.error { medicalBenefitsService.getData() }
-        return medicalBenefitsService.getData()
+    fun getTaxDataFromReceiveService(): String {
+        taxDataLoadService.getBenefitsData()
+        return taxDataLoadService.getBenefitsData().toString()
+
     }
 
 }
