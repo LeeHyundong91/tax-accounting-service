6c47ed2d3a9b1732757057109f7c85058630688c
mino <dev.minokim@gmail.com>
2023-03-09 16:54:41 +0900

    #44 mod  - add schedule

--- src/main/kotlin/net/dv/tax/TaxAccountingApplication.kt
+++ src/main/kotlin/net/dv/tax/TaxAccountingApplication.kt
@@ -4,10 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
+@EnableScheduling
 class TaxAccountingApplication{
     companion object {
         const val VERSION: String = "v1"
--- src/main/kotlin/net/dv/tax/service/common/TaxDataLoadAndSaveService.kt
+++ src/main/kotlin/net/dv/tax/service/common/TaxDataLoadAndSaveService.kt
@@ -3,10 +3,10 @@ package net.dv.tax.service.common
 import mu.KotlinLogging
 import net.dv.tax.repository.sales.*
 import net.dv.tax.service.feign.DataReceiveFeignService
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
-/*TODO ADD SCHEDULE*/
 class TaxDataLoadAndSaveService(
     private val dataReceiveFeignService: DataReceiveFeignService,
     private val medicalExamRepository: MedicalExamRepository,
@@ -15,7 +15,7 @@ class TaxDataLoadAndSaveService(
     private val employeeIndustryRepository: EmployeeIndustryRepository,
     private val salesCreditCardRepository: SalesCreditCardRepository,
     private val salesCashReceiptRepository: SalesCashReceiptRepository,
-    private val salesElecInvoiceRepository: SalesElecInvoiceRepository
+    private val salesElecInvoiceRepository: SalesElecInvoiceRepository,
 ) {
 
     private val log = KotlinLogging.logger {}
@@ -24,28 +24,42 @@ class TaxDataLoadAndSaveService(
         medicalBenefitsRepository.saveAll(dataReceiveFeignService.getMedicalBenefits())
     }
 
-    fun getCarInsuranceData(){
+    fun getCarInsuranceData() {
         carInsuranceRepository.saveAll(dataReceiveFeignService.getCarInsurance())
     }
 
-    fun getMedicalExamData(){
+    fun getMedicalExamData() {
         medicalExamRepository.saveAll(dataReceiveFeignService.getMedicalExam())
     }
 
-    fun getEmployeeIndustryData(){
+    fun getEmployeeIndustryData() {
         employeeIndustryRepository.saveAll(dataReceiveFeignService.getEmployeeIndustry())
     }
 
-    fun getSalesCreditCardData(){
+    fun getSalesCreditCardData() {
         salesCreditCardRepository.saveAll(dataReceiveFeignService.getSalesCreditCard())
     }
 
-    fun getSalesCashReceiptData(){
+    fun getSalesCashReceiptData() {
         salesCashReceiptRepository.saveAll(dataReceiveFeignService.getCashReceipt())
     }
 
-    fun getSalesElecTaxInvoiceData(){
+    fun getSalesElecTaxInvoiceData() {
         salesElecInvoiceRepository.saveAll(dataReceiveFeignService.getElecTaxInvoice())
+    }
+
+    @Throws(Exception::class)
+    @Scheduled(cron = "0 * * * * *")
+    fun callScheduleModule() {
+        log.info { "Tax Data Save In Progress ------------" }
+        medicalBenefitsRepository.saveAll(dataReceiveFeignService.getMedicalBenefits())
+        carInsuranceRepository.saveAll(dataReceiveFeignService.getCarInsurance())
+        medicalExamRepository.saveAll(dataReceiveFeignService.getMedicalExam())
+        employeeIndustryRepository.saveAll(dataReceiveFeignService.getEmployeeIndustry())
+        salesCreditCardRepository.saveAll(dataReceiveFeignService.getSalesCreditCard())
+        salesCashReceiptRepository.saveAll(dataReceiveFeignService.getCashReceipt())
+        salesElecInvoiceRepository.saveAll(dataReceiveFeignService.getElecTaxInvoice())
+        log.info { "End Of Scheduled ------------" }
     }
 
 }
