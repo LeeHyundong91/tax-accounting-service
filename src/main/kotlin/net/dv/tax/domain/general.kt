133f8887f9197803ac92070e29aa31984194470f
jupic <juphich@sugarbricks.co.kr>
2023-07-20 18:23:21 +0900

    [C] 코드 인터페이스 및 코드 컨버터 등록

---
+++ src/main/kotlin/net/dv/tax/domain/general.kt
@@ -0,0 +1,19 @@
+package net.dv.tax.domain
+
+import jakarta.persistence.AttributeConverter
+
+
+interface Code {
+    val code: String
+    val label: String
+}
+
+abstract class AbstractCodeConverter<T>(val convert: (String) -> T): AttributeConverter<T, String> where T: Enum<T>, T: Code {
+    override fun convertToDatabaseColumn(attribute: T?): String? {
+        return attribute?.code
+    }
+
+    override fun convertToEntityAttribute(code: String?): T? {
+        return code?.takeIf { it.isEmpty() }?.let { convert(code) }
+    }
+}
